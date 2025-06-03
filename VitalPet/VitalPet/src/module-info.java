f9bd8ff6eecf282b1c7292676ae904a23ca96b1f
AleVane <92054131+AleVane@users.noreply.github.com>
2025-06-02 18:47:02 -0600

    login terminado

--- VitalPet/VitalPet/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module VitalPet {
-    requires javafx.controls;
-    requires javafx.fxml;
-    exports mx.edu.uacm.is.slt.ds.vitalpet.application to javafx.graphics, javafx.fxml;
-    opens mx.edu.uacm.is.slt.ds.vitalpet.application to javafx.fxml;
-    opens mx.edu.uacm.is.slt.ds.vitalpet.controllers to javafx.fxml;
-}
--- VitalPet/VitalPet/src/mx/edu/uacm/is/slt/ds/vitalpet/strategy/ValidacionTarjetaStrategy.java
+++
@@ -1,6 +0,0 @@
-package mx.edu.uacm.is.slt.ds.vitalpet.strategy;
-
-
-public interface ValidacionTarjetaStrategy {
-    boolean validar(String numeroTarjeta, String codigoSeguridad, String fechaExpiracion);
-}
